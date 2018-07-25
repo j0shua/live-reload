78251fc14048cbec14d35271cc2086f262e22dcb
j0shua <j0shua@users.noreply.github.com>
2018-07-25 17:41:18 -0400

    adding redis to the application and docker compose (#1)

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const http = require('http');
+const redis = require('redis');
+
+const client = redis.createClient({ host: 'redis_host'});
 const port = 3000;
 
+
+client.on('error', function (err) {
+    console.error('zomg!!!:', err)
+});
+
+
+
 let count = 0;
+
 const server = http.createServer((req, res) => {
 
     console.log('got request!');
-    res.end(JSON.stringify({ status: `it works ${count++}` }));
+
+    client.set("string key", `message ${count}`, function(response) {
+        console.log("response:", response)
+        client.get("string key", function(err, result) {
+            res.end(JSON.stringify({ data: `xcount: ${count++} error: ${err && err.message}, result: ${result}` }));
+        });
+    });
 }).listen(port, (err) => {
 
     if (err) {
-        console.error('ERROR:', err);
+        console.error('ERROR:', err.message);
     }
     console.log(`server started on port ${port}`);
 });
